f27a579cda72d3eeb2d1a306be68fe9f29326fb5
Jarrod Phillips <jarrod@medivis.co>
2019-10-01 19:30:52 -0400

    Moved Mapbox init code to MapPinFragment.

--- app/src/main/java/com/example/android/maptest/MainActivity.kt
+++ app/src/main/java/com/example/android/maptest/MainActivity.kt
@@ -10,59 +10,15 @@ import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
 
-    private var mapView: MapView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        Mapbox.getInstance(application, getString(R.string.mapbox_api_key))
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MapPinFragment.newInstance())
                 .commitNow()
         }
-
-        Mapbox.getInstance(applicationContext, getString(R.string.mapbox_api_key))
         setContentView(R.layout.main_activity)
-
-        mapView = findViewById(R.id.mapView)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync {
-            map -> map.setStyle(Style.MAPBOX_STREETS)
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView?.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView?.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView?.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView?.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        mapView?.onSaveInstanceState(outState)
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView?.onLowMemory()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView?.onDestroy()
     }
 }
--- app/src/main/java/com/example/android/maptest/ui/map/MapPinFragment.kt
+++ app/src/main/java/com/example/android/maptest/ui/map/MapPinFragment.kt
@@ -2,13 +2,19 @@ package com.example.android.maptest.ui.map
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.android.maptest.R
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
 
 class MapPinFragment : Fragment() {
+
+    private var mapView: MapView? = null
 
     companion object {
         fun newInstance() = MapPinFragment()
@@ -30,6 +36,47 @@ class MapPinFragment : Fragment() {
         val viewModelFactory = MapViewModelFactory(application)
 
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(MapViewModel::class.java)
+
+        mapView = view?.findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { map ->
+            map.setStyle(Style.MAPBOX_STREETS)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
     }
 
 }
